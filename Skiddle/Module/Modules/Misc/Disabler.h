c02df11b7dbb927b8cfcb2b6cde973f9954fc4f5
mqiva <SkiddleClientBedrock@outlook.com>
2024-02-25 21:57:09 +0000

    Started Cubecraft Disabler update!

--- Skiddle/Module/Modules/Misc/Disabler.h
+++ Skiddle/Module/Modules/Misc/Disabler.h
@@ -89,13 +89,7 @@ public:
         case 1: // Sentinel
             if (TimeUtil::hasTimeElapsed("gmTick", 50, true))
             {
-                /*
-                MovePlayerPacket pkt(Game::GetLocalPlayer(), Game::GetLocalPlayer()->getPosition(), Game::GetLocalPlayer()->getMovementProxy()->getRotation(), Game::GetLocalPlayer()->getMovementProxy()->isOnGround());
-                pkt.mode = TELEPORT;
-                for (int i = 0; i < 50; i++)
-                {
-                    sender->sendToServer(&pkt);
-                }*/
+
             }
 
             // Sleep and skips
