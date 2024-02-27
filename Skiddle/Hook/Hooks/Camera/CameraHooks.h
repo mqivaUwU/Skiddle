d6b4d1c65f559be0062eea8a72df995464ce2b1a
mqiva <SkiddleClientBedrock@outlook.com>
2024-02-27 18:04:58 +0000

    Updated CameraHooks 1.20.51

--- Skiddle/Hook/Hooks/Camera/CameraHooks.h
+++ Skiddle/Hook/Hooks/Camera/CameraHooks.h
@@ -16,7 +16,7 @@ public:
     }
 
     bool Initialize() override {
-        void* addr = findSig(xorstr_("40 55 53 56 57 41 56 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 ?? 49 83 38"));
+        void* addr = findSig(xorstr_("40 55 53 56 57 41 56 48 8D 6C 24 ?? 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 ?? 49 83 38")); //1.20.51
         if (!HookFunction(addr, (void*)&CameraAvoidanceSystemUtil_tick, &oCameraAvoidanceSystemUtil_tick, xorstr_("CameraAvoidanceSystemUtil_tick"))) {
             return false;
         }
