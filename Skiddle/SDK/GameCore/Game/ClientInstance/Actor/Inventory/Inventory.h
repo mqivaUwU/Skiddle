aeb92da398edf056a5abddfa7d68edd711381cfb
mqiva <SkiddleClientBedrock@outlook.com>
2024-02-27 19:05:46 +0000

    Updated Inventory 1.20.51

--- Skiddle/SDK/GameCore/Game/ClientInstance/Actor/Inventory/Inventory.h
+++ Skiddle/SDK/GameCore/Game/ClientInstance/Actor/Inventory/Inventory.h
@@ -12,7 +12,7 @@ public:
 
 	void dropSlot(int slots)
 	{
-		static void* dropSlot = findSig("85 D2 0F 88 ? ? ? ? 48 89 5C 24 ? 55 56 57 41 54");
+		static void* dropSlot = findSig("85 D2 ? ? ? ? ? ? 48 89 5C 24 18 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 41"); //1.20.51
 		CallFunc<void, Inventory*, int, char>(
 			dropSlot,
 			this,
